df6ea98b75c171d23bf9b68f3a129776e8c92793
techiecream <34246365+techiecream@users.noreply.github.com>
2023-05-27 22:24:45 +0300

    added the predication
    Added the predication of future sales

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request
+from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
+import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
-import random
 from pymongo import MongoClient
-from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import cross_val_score
 
 app = Flask(__name__)
 
@@ -39,23 +45,187 @@ def upload():
         if uploaded_file.filename != '':
             uploaded_file.save(uploaded_file.filename)
             print("File " + uploaded_file.filename + " uploaded successfully")
-
             # Load the CSV file into a Pandas DataFrame
             df = pd.read_csv(uploaded_file.filename, encoding='utf-8')
 
-            # Rest of the data processing and visualization code
+            #df = pd.read_csv('data.csv')
+            df.head()
 
-            # Pass the data to the display page
-            columns = df.columns.tolist()
-            data = df.values.tolist()
-            bar_chart_path = 'static/bar_chart.png'
-            pie_chart_path = 'static/pie_chart.png'
+            df.describe()
 
-            return render_template('display.html', columns=columns, data=data, bar_chart_path=bar_chart_path, pie_chart_path=pie_chart_path)
+            # datatype info
+            df.info()
+
+            # find unique values
+            df.apply(lambda x: len(x.unique()))
+
+            # distplot for purchase
+            plt.style.use('fivethirtyeight')
+            plt.figure(figsize=(13, 7))
+            sns.distplot(df['Quantity'], bins=25)
+
+            # distribution of numeric variables
+            sns.countplot(df['Product_name'])
+
+            sns.countplot(df['Quantity'])
+
+            sns.countplot(df['Sales_per_week'])
+
+            sns.countplot(df['Sales_person'])
+
+            sns.countplot(df['Purchase_date'])
+
+            # check for null values
+            df.isnull().sum()
+
+            # to improve the metric use one hot encoding
+            # label encoding
+            cols = ['Product_category', 'City', 'Sales_person']
+            le = LabelEncoder()
+            for col in cols:
+                df[col] = le.fit_transform(df[col])
+            df.head()
+
+            corr = df.corr()
+            plt.figure(figsize=(14, 7))
+            sns.heatmap(corr, annot=True, cmap='coolwarm')
+            df.head()
+
+            X = df.drop(columns=['Product_id','Product_name', 'Product_category', 'Purchase_date','City_code'])
+            y = df['Quantity']
+
+            def train(model, X, y):
+                # train-test split
+                x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.25)
+                model.fit(x_train, y_train)
+
+                # predict the results
+                pred = model.predict(x_test)
+
+                # cross validation
+                cv_score = cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=5)
+                cv_score = np.abs(np.mean(cv_score))
+
+                print("Results")
+                print("MSE:", np.sqrt(mean_squared_error(y_test, pred)))
+                print("CV Score:", np.sqrt(cv_score))
+
+            model = RandomForestRegressor(n_jobs=-1)
+            train(model, X, y)
+
+            features = pd.Series(model.feature_importances_, X.columns).sort_values(ascending=False)
+            features.plot(kind='bar', title='Feature Importance')
+
+            # Define x_test before making predictions
+            x_test = df.drop(columns=['Product_id','Product_name', 'Product_category', 'Purchase_date','City_code'])
+            pred = model.predict(x_test)
+
+            # Add predicted sales to the existing 'Sales_per_week' column
+            df['Predicated_sales'] = df['Sales_per_week'] + pred
+
+            # Save the updated DataFrame to a CSV file
+            df.to_csv('submission.csv', index=False)
+
+
+            # Return the results and visualizations
+            return render_template('display.html')
 
         # Return the error message if no file is uploaded
         return render_template('upload.html', error='Error: no file uploaded')
 
+    # Return the upload page for GET requests
+    return render_template('upload.html')
+
+
+
+def clean_and_process_data(df):
+    df.head()
+
+    df.describe()
+
+    # datatype info
+    df.info()
+
+    # find unique values
+    df.apply(lambda x: len(x.unique()))
+
+    # distplot for purchase
+    plt.style.use('fivethirtyeight')
+    plt.figure(figsize=(13, 7))
+    sns.distplot(df['Quantity'], bins=25)
+
+    # distribution of numeric variables
+    sns.countplot(df['Product_name'])
+
+    sns.countplot(df['Quantity'])
+
+    sns.countplot(df['Sales_per_week'])
+
+    sns.countplot(df['Sales_person'])
+
+    sns.countplot(df['Purchase_date'])
+
+    # bivariate analysis
+    occupation_plot = df.pivot_table(index='Sales_person', values='Quantity', aggfunc=np.mean)
+    occupation_plot.plot(kind='bar', figsize=(13, 7))
+    plt.xlabel('Sales_person')
+    plt.ylabel("Quantity")
+    plt.title("Sales_person and Quantity Analysis")
+    plt.xticks(rotation=0)
+    plt.show()
+
+    # check for null values
+    df.isnull().sum()
+
+    # to improve the metric use one hot encoding
+    # label encoding
+    cols = ['Product_category', 'City', 'Sales_person']
+    le = LabelEncoder()
+    for col in cols:
+        df[col] = le.fit_transform(df[col])
+    df.head()
+
+    corr = df.corr()
+    plt.figure(figsize=(14, 7))
+    sns.heatmap(corr, annot=True, cmap='coolwarm')
+    df.head()
+
+    return df
+
+def perform_analysis(df):
+    X = df.drop(columns=['Product_id','Product_name', 'Product_category', 'Purchase_date','City_code'])
+    y = df['Quantity']
+
+    def train(model, X, y):
+        # train-test split
+        x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.25)
+        model.fit(x_train, y_train)
+
+        # predict the results
+        pred = model.predict(x_test)
+
+        # cross validation
+        cv_score = cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=5)
+        cv_score = np.abs(np.mean(cv_score))
+
+        print("Results")
+        print("MSE:", np.sqrt(mean_squared_error(y_test, pred)))
+        print("CV Score:", np.sqrt(cv_score))
+
+    model = RandomForestRegressor(n_jobs=-1)
+    train(model, X, y)
+
+    features = pd.Series(model.feature_importances_, X.columns).sort_values(ascending=False)
+    features.plot(kind='bar', title='Feature Importance')
+
+    # Define x_test before making predictions
+    x_test = df.drop(columns=['Product_id','Product_name', 'Product_category', 'Purchase_date','City_code'])
+    pred = model.predict(x_test)
+
+    submission = pd.DataFrame()
+    submission['Product_id'] = df['Product_id']
+    submission['Sales_per_week'] = pred
+    submission.to_csv('submission.csv', index=False)
 
 
 
@@ -99,4 +269,4 @@ def products():
     return render_template('liproducts.html',  product_list=product_list)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.43.52',port=5000)
+    app.run(debug=True ,port=5000)
